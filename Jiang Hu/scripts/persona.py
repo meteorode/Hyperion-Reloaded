d7e95e99629056a2e6207c950bbc43a5141b768e
meteor <songge@gmail.com>
2021-02-26 11:30:40 +0800

    Update persona.py

--- Jiang Hu/scripts/persona.py
+++ Jiang Hu/scripts/persona.py
@@ -1,5 +1,30 @@
 # persona.py
 
+# Initilization
+
+import spacy
+from collections import Counter
+#from tabulate import tabulate
+from pathlib import Path
+
+p = Path('.')   # current Path
+
+# 杯雪
+beixue = list(p.glob('books/beixue/*.txt'))
+beixue.sort()
+
+# spacy.prefer_gpu()  # Using GPU to run programm
+
+#nlp = spacy.load("zh_core_web_lg")
+nlp = spacy.load('zh_core_web_trf') # spacy 3.0 stable model.
+
+def read_chapters(book):
+    text = ''
+    for chapter in book:
+        with open(chapter, 'r') as file:
+            text += file.read()
+    return text
+
 # Part I: Sentiment Analysis to determine A char's 「心」traits
 
 # Part II: Skill analysis
@@ -7,3 +32,12 @@
 # Part III: Body measurement traits analysis
 
 # Part IV: Intelligence analysis
+
+# Test units here.
+
+def test(): 
+    cup_snow = read_chapters(beixue)
+    cup_snow_doc = nlp(cup_snow)
+    print('finished!')
+
+test()
