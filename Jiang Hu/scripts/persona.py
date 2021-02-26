27c2df91e7eb4aca8a0ef4858d59a614d46cbea4
meteor <songge@gmail.com>
2021-02-26 17:04:00 +0800

    Update persona.py

--- Jiang Hu/scripts/persona.py
+++ Jiang Hu/scripts/persona.py
@@ -6,8 +6,59 @@ import spacy
 from collections import Counter
 #from tabulate import tabulate
 from pathlib import Path
+import time
 
 p = Path('.')   # current Path
+
+# 飞雪连天射白鹿，笑书神侠倚碧鸳
+
+feihu = list(p.glob('novels/jinyong/feihuwaizhuan/*.txt'))
+feihu.sort()
+
+xueshan = list(p.glob('novels/jinyong/xueshanfeihu/*.txt'))
+xueshan.sort()
+
+liancheng = list(p.glob('novels/jinyong/lianchengjue/*.txt'))
+liancheng.sort()
+
+tianlong = list(p.glob('novels/jinyong/tianlongbabu/*.txt'))
+tianlong.sort()
+
+shediao = list(p.glob('novels/jinyong/shediaoyingxiongzhuan/*.txt'))
+shediao.sort()
+
+baima = list(p.glob('novels/jinyong/baimaxiaoxifeng/*.txt'))
+baima.sort()
+
+luding = list(p.glob('novels/jinyong/ludingji/*.txt'))
+luding.sort()
+
+xiaoao = list(p.glob('novels/jinyong/xiaoaojianghu/*.txt'))
+xiaoao.sort()
+
+shujian = list(p.glob('novels/jinyong/shujianenchoulu/*.txt'))
+shujian.sort()
+
+shendiao = list(p.glob('novels/jinyong/shendiaoxialv/*.txt'))
+shendiao.sort()
+
+xiakexing = list(p.glob('novels/jinyong/xiakexing/*.txt'))
+xiakexing.sort()
+
+yitian = list(p.glob('novels/jinyong/yitiantulongji/*.txt'))
+yitian.sort()
+
+bixuejian = list(p.glob('novels/jinyong/bixuejian/*.txt'))
+bixuejian.sort()
+
+yuanyang = list(p.glob('novels/jinyong/yuanyangdao/*.txt'))
+yuanyang.sort()
+
+person_names = []
+with open('novels/jinyong/person_list.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        person_names.append(line.rstrip('\n'))
 
 # 杯雪
 beixue = list(p.glob('books/beixue/*.txt'))
@@ -25,6 +76,17 @@ def read_chapters(book):
             txts.append(file.read())
     return txts
 
+# Basic Statistics methods
+
+def count_ents(doc):
+    entities = {}
+    for ent in doc.ents:
+        if ent.text in entities:
+            entities[ent.text] += 1
+        else:
+            entities[ent.text] = 1
+    print("top entities{}".format(sorted(entities.items(), key=lambda kv: kv[1], reverse=True)[:30]))
+
 # Part I: Sentiment Analysis to determine A char's 「心」traits
 
 # Part II: Skill analysis
@@ -39,6 +101,6 @@ def test():
     cup_snow = read_chapters(beixue)
     for cs in cup_snow:
         cup_snow_doc = nlp(cs)
-    print('finished!')
+        count_ents(cup_snow_doc)
 
 test()
