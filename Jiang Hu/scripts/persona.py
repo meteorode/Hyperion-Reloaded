3bfed53911bcce465c969b734a61bd5622fadd38
meteor <songge@gmail.com>
2021-03-04 14:14:07 +0800

    Update persona.py

--- Jiang Hu/scripts/persona.py
+++ Jiang Hu/scripts/persona.py
@@ -112,11 +112,11 @@ def count_big_names(names, docs, count_num): # Count most common names from docs
                         name_freq[name] += 1
                     else:
                         name_freq[name] = 1
-    name_freq = sorted(name_freq.items(), key=lambda kv: kv[1], reverse=True)
-    if len(name_freq) >= count_num:
-        return name_freq[:count_num]
+    name_tuple = sorted(name_freq.items(), key=lambda kv: kv[1], reverse=True)
+    if len(name_tuple) >= count_num:
+        return dict(name_tuple[:count_num])
     else:
-        return name_freq
+        return dict(name_tuple)
 
 def count_attrs(doc, attr_type):
     entities = {}
@@ -160,32 +160,32 @@ def calc_distance(token, leaf): # Assert leaf is another token in the same sente
 
     return dis
 
-def word_cloud(name, doc, pos_types, dep_types): # Calc polarity_value with token's related words.
+def word_cloud(name, docs, pos_types, dep_types): # Calc polarity_value with token's related words.
     cloud = {}
     hourglass = {'pleasantness': 0, 'attention': 0, 'sensitivity': 0, 'aptitude': 0}
     total = 1
-    for token in doc:
-        if name in token.text:
-            total += 1
-            for leaf in token.sent:
-                #print (leaf.text, calc_distance(token, leaf))
-                if leaf.pos_ in pos_types or leaf.dep_ in dep_types:
-                    if leaf.text in cloud:
-                        try:
-                            cloud[leaf.text] += calc_distance(token, leaf) * cn_sn.polarity_value(leaf.text)
-                            sentics = cn_sn.sentics(leaf.text)
-                            for s in sentics:
-                                hourglass[s] += calc_distance(token, leaf) * sentics[s]
-                        except:
-                            cloud[leaf.text] += calc_distance(token, leaf) * 0.01 # If no senticnet data, multipled by a minor 
-                    else:
-                        try:
-                            cloud[leaf.text] = calc_distance(token, leaf) * cn_sn.polarity_value(leaf.text)
-                            sentics = cn_sn.sentics(leaf.text)
-                            for s in sentics:
-                                hourglass[s] += calc_distance(token, leaf) * sentics[s]
-                        except:
-                            cloud[leaf.text] = calc_distance(token, leaf) * 0.01 # If no senticnet data, multipled by a minor
+    for doc in docs:
+        for token in doc:
+            if name in token.text:
+                for leaf in token.sent:
+                    #print (leaf.text, calc_distance(token, leaf))
+                    if leaf.pos_ in pos_types or leaf.dep_ in dep_types:
+                        if leaf.text in cloud:
+                            try:
+                                cloud[leaf.text] += calc_distance(token, leaf) * cn_sn.polarity_value(leaf.text)
+                                sentics = cn_sn.sentics(leaf.text)
+                                for s in sentics:
+                                    hourglass[s] += calc_distance(token, leaf) * sentics[s]
+                            except:
+                                cloud[leaf.text] += calc_distance(token, leaf) * 0.01 # If no senticnet data, multipled by a minor 
+                        else:
+                            try:
+                                cloud[leaf.text] = calc_distance(token, leaf) * cn_sn.polarity_value(leaf.text)
+                                sentics = cn_sn.sentics(leaf.text)
+                                for s in sentics:
+                                    hourglass[s] += calc_distance(token, leaf) * sentics[s]
+                            except:
+                                cloud[leaf.text] = calc_distance(token, leaf) * 0.01 # If no senticnet data, multipled by a minor
     return [cloud, hourglass, total]
 
 # Part I: Sentiment Analysis to determine A char's 「心」traits
@@ -199,13 +199,9 @@ def hourglass_analysis(book_name, docs, names):   # docs shoule be the nlp parsi
     hourglass_with_names = {}
     for name in names:
         result = {'pleasantness': 0, 'attention': 0, 'sensitivity': 0, 'aptitude': 0}
-        sent_count = 1
-        for doc in docs:
-            temp_wc = word_cloud(name, doc, ['ADJ', 'NOUN', 'VERB'], ['amod', 'dobj', 'pobj'])
-            temp_result = temp_wc[1]
-            sent_count += temp_wc[2]
-            for tr in temp_result:
-                result[tr] += temp_result[tr]
+        wc_with_names = word_cloud(name, docs, ['ADJ', 'NOUN', 'VERB'], ['amod', 'dobj', 'pobj'])
+        result = wc_with_names[1]
+        sent_count = wc_with_names[2]
         for r in result:
             result[r] = result[r] / sent_count
         hourglass_with_names[name] = result
@@ -238,8 +234,7 @@ def test():
     shendiao_docs = []
     for txt in shendiao_txts:
         shendiao_docs.append(nlp(txt))
-    shendiao_names = count_big_names(jinyong_names, shendiao_docs, 20)
-    print(shendiao_names)
-    #hourglass_analysis(shendiao, shendiao_names)
+    shendiao_names = list(count_big_names(jinyong_names, shendiao_docs, 20))
+    hourglass_analysis("shendiao", shendiao_docs, shendiao_names)
 
 test()
