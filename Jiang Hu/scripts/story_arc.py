5794016c929bc21e2c2c1de54c871e8dc40ba5ba
Meteor <songge@gmail.com>
2021-03-09 09:12:56 +0800

    Create story_arc.py

---
+++ Jiang Hu/scripts/story_arc.py
@@ -0,0 +1,8 @@
+# story_arc
+
+# Template from Wuxia novels, Greek mythology, Shakespeare, etc
+# Most of them would be defined like:
+#   choices = [c1, c2, ..., cn]
+#   if f(hero.persona) ==/!=/>/< sth:
+#       hero.choice = c(h)
+#   action_lists = execute(hero.choice)
