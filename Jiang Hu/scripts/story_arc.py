35c4cb6c775af774f785fff4988e6294d537d584
Meteor <songge@gmail.com>
2021-03-09 23:47:25 +0800

    Update story_arc.py

--- Jiang Hu/scripts/story_arc.py
+++ Jiang Hu/scripts/story_arc.py
@@ -7,6 +7,29 @@
 #       hero.choice = c(h)
 #   action_lists = execute(hero.choice)
 
+import spacy
+from pathlib import Path
+from senticnet.senticnet import SenticNet
+from senticnet.babelsenticnet import BabelSenticNet
+import inspect
+import math
+import numpy as np
+import persona
+
+# spaCy init
+
+spacy.prefer_gpu()  # Using GPU to run programm
+
+nlp = spacy.load('zh_core_web_trf') # spacy 3.0 stable model.
+en_nlp = spacy.load('en_core_web_trf')
+
+sn = SenticNet()
+cn_sn = BabelSenticNet('cn')    # Use SenticNet to analysis.
+
+p = Path('.')   # current Path
+
+# Define heroes
+
 class hero:
     def __init__(self, name ='吴茗', persona_ocean = {'Openness': 0.5, 'Consientiousness': 0.5, 'Extraversion': 0.5, 
     'Agreebleness': 0.5, 'Neuroticsm': 0.5}, persona_hourglass = {'pleasantness': 0, 'attention': 0, 
@@ -15,9 +38,17 @@ class hero:
         self.persona_ocean = persona_ocean
         self.persona_hourglass = persona_hourglass
 
+# Propp models
+propp_models = {'Absentation': '一位家庭成员离家外出'}
+
 # Test Unit
 def test():
-    h = hero('郭靖')
-    print(h.persona_hourglass)
+    for model_name in propp_models:
+        model_content = propp_models[model_name]
+        doc = nlp(model_content)
+        for token in doc:
+            print(token.text, token.dep_, token.ent_type_, token.pos_)
+            if persona.has_sentic(token.text):
+                print(persona.word_transformer(token.text))
 
 test()
