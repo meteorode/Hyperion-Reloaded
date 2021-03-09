4a13b768c54ec0b573709a2b71f6d122e1c1c016
Meteor <songge@gmail.com>
2021-03-09 10:02:40 +0800

    Update story_arc.py

--- Jiang Hu/scripts/story_arc.py
+++ Jiang Hu/scripts/story_arc.py
@@ -6,3 +6,18 @@
 #   if f(hero.persona) ==/!=/>/< sth:
 #       hero.choice = c(h)
 #   action_lists = execute(hero.choice)
+
+class hero:
+    def __init__(self, name ='吴茗', persona_ocean = {'Openness': 0.5, 'Consientiousness': 0.5, 'Extraversion': 0.5, 
+    'Agreebleness': 0.5, 'Neuroticsm': 0.5}, persona_hourglass = {'pleasantness': 0, 'attention': 0, 
+    'sensitivity': 0, 'aptitude': 0}):
+        self.name = name
+        self.persona_ocean = persona_ocean
+        self.persona_hourglass = persona_hourglass
+
+# Test Unit
+def test():
+    h = hero('郭靖')
+    print(h.persona_hourglass)
+
+test()
