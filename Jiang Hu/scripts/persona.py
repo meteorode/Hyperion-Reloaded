51595f2dcfbd93375f7cecd92874f7d5d5170d7f
meteor <songge@gmail.com>
2021-03-14 21:42:07 +0800

    Spilt books  and minor changes.

--- Jiang Hu/scripts/persona.py
+++ Jiang Hu/scripts/persona.py
@@ -12,6 +12,7 @@ from senticnet.babelsenticnet import BabelSenticNet
 import inspect
 import math
 import numpy as np
+import books
 
 spacy.prefer_gpu()  # Using GPU to run programm
 
@@ -23,58 +24,6 @@ sn = SenticNet()
 cn_sn = BabelSenticNet('cn')    # Use SenticNet to analysis.
 
 p = Path('.')   # current Path
-
-# 飞雪连天射白鹿，笑书神侠倚碧鸳
-
-feihu = list(p.glob('novels/jinyong/feihuwaizhuan/*.txt'))
-feihu.sort()
-
-xueshan = list(p.glob('novels/jinyong/xueshanfeihu/*.txt'))
-xueshan.sort()
-
-liancheng = list(p.glob('novels/jinyong/lianchengjue/*.txt'))
-liancheng.sort()
-
-tianlong = list(p.glob('novels/jinyong/tianlongbabu/*.txt'))
-tianlong.sort()
-
-shediao = list(p.glob('novels/jinyong/shediaoyingxiongzhuan/*.txt'))
-shediao.sort()
-
-baima = list(p.glob('novels/jinyong/baimaxiaoxifeng/*.txt'))
-baima.sort()
-
-luding = list(p.glob('novels/jinyong/ludingji/*.txt'))
-luding.sort()
-
-xiaoao = list(p.glob('novels/jinyong/xiaoaojianghu/*.txt'))
-xiaoao.sort()
-
-shujian = list(p.glob('novels/jinyong/shujianenchoulu/*.txt'))
-shujian.sort()
-
-shendiao = list(p.glob('novels/jinyong/shendiaoxialv/*.txt'))
-shendiao.sort()
-
-xiakexing = list(p.glob('novels/jinyong/xiakexing/*.txt'))
-xiakexing.sort()
-
-yitian = list(p.glob('novels/jinyong/yitiantulongji/*.txt'))
-yitian.sort()
-
-bixuejian = list(p.glob('novels/jinyong/bixuejian/*.txt'))
-bixuejian.sort()
-
-yuanyang = list(p.glob('novels/jinyong/yuanyangdao/*.txt'))
-yuanyang.sort()
-
-# 小李飞刀成绝响，人间不见楚留香
-
-dashamo = ['novels/gulong/chuliuxiang-dashamo.txt']
-
-# 杯雪
-beixue = list(p.glob('books/beixue/*.txt'))
-beixue.sort()
 
 jinyong_names = []
 with open('novels/jinyong/person_list.txt', 'r') as file:
--- Jiang Hu/scripts/story_arc.py
+++ Jiang Hu/scripts/story_arc.py
@@ -14,7 +14,8 @@ from senticnet.babelsenticnet import BabelSenticNet
 import inspect
 import math
 import numpy as np
-import persona
+#import persona
+import books
 from sentence_transformers import SentenceTransformer, util
 import torch
 
@@ -23,21 +24,41 @@ import torch
 spacy.prefer_gpu()  # Using GPU to run programm
 
 nlp = spacy.load('zh_core_web_trf') # spacy 3.0 stable model.
-#en_nlp = spacy.load('en_core_web_trf')
+en_nlp = spacy.load('en_core_web_trf')
 
 sn = SenticNet()
 cn_sn = BabelSenticNet('cn')    # Use SenticNet to analysis.
 
 p = Path('.')   # current Path
 
+embedder = SentenceTransformer('./models/distiluse-base-multilingual-cased')
+
 # Fundamental modules
+
+# Methods to get var name as string.
+
+def read_chapters(book):
+    txts = []
+    for chapter in book:
+        with open(chapter, 'r') as file:
+            txts.append(file.read())
+    return txts
+
+def retrieve_name(var):
+        """
+        Gets the name of var. Does it from the out most frame inner-wards.
+        :param var: variable to get name from.
+        :return: string
+        """
+        for fi in reversed(inspect.stack()):
+            names = [var_name for var_name, var_val in fi.frame.f_locals.items() if var_val is var]
+            if len(names) > 0:
+                return names[0]
 
 def sort_dict(dict): # return a sorted dictionary by values
     sorted_tuples = sorted(dict.items(), key=lambda item:item[1], reverse=True)   # return a sorted tuple by lambda function
     sorted_dict = {k: v for k, v in sorted_tuples}
     return sorted_dict
-
-embedder = SentenceTransformer('./models/distiluse-base-multilingual-cased')
 
 def word_similarity(w1, w2):    # Use model.encode() and pytorch_cos_sim() to calc
     emb1 = embedder.encode(w1)
@@ -138,6 +159,7 @@ def script_extractor(doc): # extract scripts-like information from doc
                 dixit = trim_conversation(sent.text)
                 script = nsubj + ' SAY: ' + dixit
                 scripts.append(script)
+                break
             elif token.ent_type_ in ['GPE', 'LOC', 'PRODUCT', 'MONEY', 'WORK_OF_ART', 'EVENT']: # MOVE TO/GAIN/ATTEND
                 ent = token.ent_type_
                 dobj = token.text
@@ -152,6 +174,7 @@ def script_extractor(doc): # extract scripts-like information from doc
                     nsubj = '宋兵乙'
                 script = nsubj + ent_table[ent] + dobj
                 scripts.append(script)
+                break
             elif token.ent_type_ == 'PERSON' and token.dep_ == 'nsubj':
                 action_related = action_classify(token.head.text)
                 if (action_related in ['talk', 'kill', 'fight', 'defeat']):
@@ -165,6 +188,7 @@ def script_extractor(doc): # extract scripts-like information from doc
                     action_table = {'talk': ' TALK TO: ', 'defeat': ' DEFEAT: ', 'fight': ' FIGHT WITH: ', 'kill': ' KILL: '}
                     script = nsubj + action_table[action_related] + dobj
                     scripts.append(script)
+                    break
     return scripts
 
 # Semantic Search based on sentence transformer
@@ -185,28 +209,36 @@ def semantic_search(corpus, queries, result_num): # # Find the closest {result_n
         for score, idx in zip(top_results[0], top_results[1]):
             print(corpus[idx], "(Score: {:.4f})".format(score))
 
-# Test Unit
-def test():
-    txts = persona.read_chapters(persona.shendiao)
+def write_script(book_name, slice_length, doc_type):  # Write scipts to files, slice the docs to increase performance
+    txts = read_chapters(book_name)
     print('===Finish Reading===\n')
-    book_name = 'shendiao'
-    txts_num = len(txts)
-    if txts_num >= 6:   # Magic Number!
-        part_num = int(txts_num/6) + 1
-    for i in range(part_num):
+    book_name_text = retrieve_name(book_name)
+    chapters_num = len(txts)
+    if chapters_num >= slice_length:
+        parts_num = int(chapters_num/slice_length) + 1
+    else:
+        parts_num = 1
+    for i in range(parts_num):
         all_cmds = []
-        for p in range(6):
-            txt = txts[min(i*6 + p, txts_num-1)]
-            doc = nlp(txt)
-            print('==spaCy NLP done!==\n')
+        for p in range(min(slice_length, chapters_num)):
+            txt = txts[min(i*3+p, chapters_num-1)]
+            if doc_type == 'cn':
+                doc = nlp(txt)
+            elif doc_type == 'en':
+                dco = en_nlp(txt)
+            print('===spaCy NLP done!===\n')
             cmds = script_extractor(doc)
-            print('==Chapter %d parsed.==\n' %(txts.index(txt)+1) )
+            print('===Chapter %d parsed!===\n' %(txts.index(txt)+1))
             all_cmds.append(cmds)
-        with open('%s_timeline_part%d.txt' %(book_name, i), 'w+') as file:
+        with open('%s_timeline_part%d.txt' %(book_name_text, i), 'w+') as file:
             for cmds in all_cmds:
                 for cmd in cmds:
                     file.write(cmd + '\n')
                 print('==File part%d write finished\n'%(i+1))
+
+# Test Unit
+def test():
+    write_script(books.shediao, 3, 'cn')
     #docs.append(nlp(txt))
     #doc_milestone = list(persona.find_sents_with_specs(docs, ['PERSON', 'LOC', 'GPE', 'EVENT'])[1].values())
     #queries = list(propp_models.values())
