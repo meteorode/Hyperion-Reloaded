495b4c7ca8d5a1126456f9f6a96d32e3db685006
meteor <songge@gmail.com>
2022-03-11 10:00:44 +0800

    Update Worldle.py

--- Storytelling/Worldle.py
+++ Storytelling/Worldle.py
@@ -1,10 +1,10 @@
 # Worldle.py
 
 # Construct a game world, and let NPCs acting as PCs
-# V0.1 simple world and simple rules for NPCcs.
+# V0.1 simple world and simple rules for NPCs.
 
-# Attrs:
-# Actions:
-# Locations:
-# NPCs:
-# Objs:
+# Attrs: Name, Age, Gender, HP, ATK, [relations], etc
+# Actions: Talk, Fight(Kill), Show Love, Marry, Find(Objs)
+# Locations: Homes(All NPCs'), Village, Wild
+# NPCs: 3 random NPCs
+# Objs: Treasure, Weapon, Poison, BE, HE.
