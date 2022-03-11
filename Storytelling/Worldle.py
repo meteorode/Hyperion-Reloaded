5a4df58d1c6442d235a266750436839a48c16804
meteor <songge@gmail.com>
2022-03-11 11:11:34 +0800

    Update Worldle.py

--- Storytelling/Worldle.py
+++ Storytelling/Worldle.py
@@ -4,7 +4,11 @@
 # V0.1 simple world and simple rules for NPCs.
 
 # Attrs: Name, Age, Gender, HP, ATK, [relations], etc
-# Actions: Talk, Fight(Kill), Show Love, Marry, Find(Objs)
+# Actions: Move, Talk, Fight(Kill), Show Love, Marry, Find(Objs)
 # Locations: Homes(All NPCs'), Village, Wild
 # NPCs: 3 random NPCs
 # Objs: Treasure, Weapon, Poison, BE, HE.
+
+# Game loop:
+#   - 
+#
