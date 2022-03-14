8afc8f4acd03430b60cdee38a02d99cdd4b79e82
meteor <songge@gmail.com>
2022-03-14 17:31:23 +0800

    Add first version of two NPCs interact with each other

--- Storytelling/Worldle.py
+++ Storytelling/Worldle.py
@@ -41,6 +41,14 @@ init_locations = ['村镇', '市集', '郊外']
 for inpc in init_npcs:
     init_locations.append(inpc.location)
 
+def search_npc_by_name(npcs, name):   # as title.
+    for npc in npcs:
+        if npc.name == name:
+            return npc
+
+def take_two(npc_one, npc_two): # Two npcs do something together.
+    print (npc_one.name + ' 与 ' + npc_two.name + ' 进行了一番友好的交谈')    # _TO_BE_UPDATED_
+
 class Game: # Global vars in a game, like year, day, hour, is_end, etc
     def __init__(self, year=0, day=0, hour=0, is_over=False, npcs=[], locs=[]):
         self.year = year
@@ -90,7 +98,19 @@ def life_game(): # One game loop
         for npc in new_game.npcs:   # NPCs act in turn.        
             npc.location = choice(init_locations)   # Everyone has their destination.
             loc_with_npc[npc.location].append(npc.name)
-        #print(loc_with_npc)
+        for loc in loc_with_npc:
+            if len(loc_with_npc[loc]) == 1: # Only one NPC, do something single.
+                pass
+            elif len(loc_with_npc[loc]) > 1:    # Multiple npcs, they will interact with others.
+                names = loc_with_npc[loc]
+                for name in names:
+                    current_npc = search_npc_by_name(new_game.npcs, name)
+                    temp_names = names.copy()
+                    temp_names.remove(name) # maybe there've some better way to do this?
+                    for tn in temp_names:
+                        next_npc = search_npc_by_name(new_game.npcs, tn)
+                        if (current_npc != None) & (next_npc != None):
+                            take_two(current_npc, next_npc) # Do something with the other npc
         if (len(new_game.npcs) == 1):
             new_game.is_over = True
     print('Game Over!')
